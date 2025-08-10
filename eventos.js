d42e644562ab163d8900d0bdf5cb4511a308a875
Steewar Martinez <steewarmartinez@gmail.com>
2025-08-10 17:14:50 -0300

    Evitar la propagacion

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.getElementsByClassName("container")[0];
+const btn = div.querySelector("button");
 
 div.addEventListener("click", function () {
   alert("Hola! Soy el div");
+});
+
+btn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  alert("Hola!");
 });
